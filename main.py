62b741e261bdac35a1355e8ad9d6e0a1ed203887
Ilya <fea.wf@bk.ru>
2023-05-15 20:14:09 +0700

    -добавил функцию сложения введеных целых чисел -при запуске файла main изменил запуск функции на plus

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
+def plus():
+    a = int(input())
+    b = int(input())
+    q = a+b
+    print(q)
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    plus()
